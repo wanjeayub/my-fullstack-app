6794b93705e5272be6ee938f7498f0b9d1c502a1
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-31 09:17:57 +0300

    add backend url

--- frontend-test/src/api.js
+++ frontend-test/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://your-backend-url.vercel.app/api";
+const API_URL = "https://backend-test-omega-olive.vercel.app/";
 
 export async function fetchMessage() {
   const response = await fetch(`${API_URL}`);
